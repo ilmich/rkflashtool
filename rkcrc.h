c922af44a557cf84f236c2a23aabf216a3e3390d
ilmich <ardutu@gmail.com>
2023-04-20 10:22:37 +0200

    commonize rc4 algo

--- rkcrc.h
+++ rkcrc.h
@@ -162,8 +162,7 @@ static uint32_t crc32table[] = {
 	0xbcbb966d, 0xb87a9bda, 0xb5398d03, 0xb1f880b4,
 };
 
-static inline uint16_t
-rkcrc16(uint16_t crc, uint8_t *buf, uint64_t size)
+static inline uint16_t rkcrc16(uint16_t crc, uint8_t *buf, uint64_t size)
 {
 
 	while (size-- > 0)
@@ -172,14 +171,47 @@ rkcrc16(uint16_t crc, uint8_t *buf, uint64_t size)
 	return crc;
 }
 
-static inline uint32_t
-rkcrc32(uint32_t crc, uint8_t *buf, uint64_t size)
+static inline uint32_t rkcrc32(uint32_t crc, uint8_t *buf, uint64_t size)
 {
 
 	while (size-- > 0)
 		crc = (crc << 8) ^ crc32table[(crc >> 24) ^ *buf++];
 
 	return crc;
+}
+
+static inline void rkrc4(unsigned char* buf, unsigned short len)
+{
+    unsigned char S[256],K[256],temp;
+    unsigned short i,j,t,x;
+    unsigned char key[16]={124,78,3,4,85,5,9,7,45,44,123,56,23,13,23,17};
+
+    j = 0;
+    for(i=0; i<256; i++){
+        S[i] = (unsigned char)i;
+        j&=0x0f;
+        K[i] = key[j];
+        j++;
+    }
+
+    j = 0;
+    for(i=0; i<256; i++){
+        j = (j + S[i] + K[i]) % 256;
+        temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+    }
+
+    i = j = 0;
+    for(x=0; x<len; x++){
+        i = (i+1) % 256;
+        j = (j + S[i]) % 256;
+        temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+        t = (S[i] + (S[j] % 256)) % 256;
+        buf[x] = buf[x] ^ S[t];
+    }
 }
 
 #endif /* !_RKCRC_H_ */
